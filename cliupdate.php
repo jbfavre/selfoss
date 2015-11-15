9e89784a5b9704fb257da2b60976b44cc04401c2
Jean Baptiste Favre <debian@jbfavre.org>
2015-11-15 17:22:23 +0100

    Allows to get different log level for cliupdate

--- cliupdate.php
+++ cliupdate.php
@@ -4,6 +4,12 @@ require('config.php');
 chdir(__DIR__);
 require(__DIR__.'/common.php');
 
+$f3->set('DEBUG',0);
+$f3->set(
+    'logger',
+    new \helpers\Logger( \F3::get('LOGS_DIR').'/default.log', $f3->get('logger_level') )
+);
+
 $f3->set('FTRSS_DATA_DIR', __dir__.'/data/fulltextrss');
 
 $loader = new \helpers\ContentLoader();
